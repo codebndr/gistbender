0666db00c3619780f31ec8dbe8f6ec5d8238da56
Ryan McClure <ryanmcclure4@gmail.com>
2015-10-24 12:21:15 -0700

    getLink button now generates db entry and fetches unique url string

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,8 +1,10 @@
+from bson import SON
+from bson.objectid import ObjectId
 import os
 import glob
 
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request
 
 __all__ = [os.path.basename(f)[:-3]
            for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
@@ -10,3 +12,15 @@ __all__ = [os.path.basename(f)[:-3]
 @app.route('/')
 def main():
   return render_template('index.html')
+
+@app.route('/<sketch_url>', methods=['GET'])
+def view_sketch(sketch_url):
+    sketch = db.sketches.find_one({
+        'url': sketch_url
+    })  
+    data = {'url': sketch['url'],
+            'filename': sketch['filename'],
+            'description': sketch['description'],
+            'code': sketch['code']
+    }   
+    return render_template('index.html', name=data['url'], code=data['code'])
